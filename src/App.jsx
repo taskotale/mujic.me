0f3b26725a84aba29cfec749b08a56bdbdf112f9
taskotale <110062804+taskotale@users.noreply.github.com>
2023-10-30 14:38:39 -0400

    solving vercel problem with deployment, cant resolve because of name

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Landing from "./components/Landing.jsx"
-import SideNav from "./components/sideNav.jsx"
+import SideNav from "./components/SideNav.jsx"
+
 function App() {
 
   return (
     <>
        <Landing />
-       <SideNav side='right'/>
-       <SideNav side='left'/>
        <SideNav side='top'/>
+       <SideNav side='right'/>
        <SideNav side='bottom'/>
+       <SideNav side='left'/>
     </>
   )
 }
