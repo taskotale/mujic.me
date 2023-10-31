ff49aa9762c1abc04e992deb904c3052ff75a3ca
taskotale <110062804+taskotale@users.noreply.github.com>
2023-10-30 20:17:34 -0400

    changed bg style

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -1,34 +1,33 @@
 import { useState } from "react";
-import "../styles/testSide.css"
+import "../styles/cube.css"
 import Landing from "./Landing";
 
 export default function TestSide () {
     
-    const [showSide, setShowSide] = useState('')
-
-    let cubeClass = 'cube ' + showSide
+    const [showSide, setShowSide] = useState('cube show-')
 
     const changeSide = (side) => {
-        setShowSide('show-' + side)
+        setShowSide('cube show-' + side)
     }
 
     return (
         <>
         <div className="scene">
-            <div className={cubeClass} >
+            <div className={showSide} >
                 <div className="cube__face cube__face--front"><Landing/></div>
-                <div className="cube__face cube__face--back">back</div>
-                <div className="cube__face cube__face--right">right</div>
-                <div className="cube__face cube__face--left">left</div>
+                {/* removed back side for now*/}
+                {/* <div className="cube__face cube__face--back">back</div> */}
+                <div className="cube__face cube__face--right">Projects</div>
+                <div className="cube__face cube__face--left">About Me</div>
                 <div className="cube__face cube__face--top">top</div>
                 <div className="cube__face cube__face--bottom">bottom</div>
             </div>
             </div>
             <div>
-                <button name="rotate-cube-side" onClick={e=>changeSide(e.target.value)} value="front"> front </button>
-                <button name="rotate-cube-side" onClick={e=>changeSide(e.target.value)} value="right"> right </button>
-                <button name="rotate-cube-side" onClick={e=>changeSide(e.target.value)} value="back" > back </button>
-                <button name="rotate-cube-side" onClick={e=>changeSide(e.target.value)} value="left" > left </button>
+                <button name="rotate-cube-side" onClick={e=>changeSide(e.target.value)} value="front"> Start </button>
+                <button name="rotate-cube-side" onClick={e=>changeSide(e.target.value)} value="right"> Projects </button>
+                {/* <button name="rotate-cube-side" onClick={e=>changeSide(e.target.value)} value="back" > back </button> */}
+                <button name="rotate-cube-side" onClick={e=>changeSide(e.target.value)} value="left" > About Me </button>
                 <button name="rotate-cube-side" onClick={e=>changeSide(e.target.value)} value="top" > top </button>
                 <button name="rotate-cube-side" onClick={e=>changeSide(e.target.value)} value="bottom" > bottom  </button>
             </div>
