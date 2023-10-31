dcfb366102e5e2786f536c015bad3878696044c9
taskotale <110062804+taskotale@users.noreply.github.com>
2023-10-31 18:00:42 -0400

    changed style of modal

--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -1,7 +1,6 @@
 import { useRef } from "react"
 import '../styles/modal.css'
-import { object } from "prop-types"
-import { func } from "prop-types"
+import { object, func } from "prop-types"
 
 export default function Modal ({content, closeModal}) {
     const outsideDiv = useRef(null)
