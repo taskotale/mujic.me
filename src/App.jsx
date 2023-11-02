158f27652c24c600327d37cdabac87dbe6f76dc6
taskotale <110062804+taskotale@users.noreply.github.com>
2023-11-02 03:16:02 -0400

    changed buttons to radio in project to be able to keep selected

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,39 @@
-import { useEffect, useState, useRef } from "react"
-import Landing from "./components/Landing.jsx"
-import SideNav from "./components/SideNav.jsx"
-import Projects from "./components/Projects.jsx"
-import Modal from "./components/Modal.jsx"
+import { useEffect, useState, useRef } from 'react';
+import Landing from './components/Landing.jsx';
+import SideNav from './components/SideNav.jsx';
+import Projects from './components/Projects.jsx';
+import Modal from './components/Modal.jsx';
 
 function App() {
-  const [modal, setModal] = useState(null)
-  const ref1 = useRef(null)
-  const ref2 = useRef(null)
-  const ref3 = useRef(null)
-  const ref4 = useRef(null)
+	const [modal, setModal] = useState(null);
+	const ref1 = useRef(null);
+	const ref2 = useRef(null);
+	const ref3 = useRef(null);
+	const ref4 = useRef(null);
 
-  return (
-    <>
-    {modal && <Modal content={modal} closeModal={setModal}/>}
-      <div
-        onClick={()=>ref1.current.scrollIntoView({behavior:'smooth'})} 
-        ref={ref1} >
-        <Landing />
-      </div>
-      <div className="side-btns">
-        <SideNav
-           side='right'
-           goTo={ref2}
-        />
-        <SideNav
-           side='bottom'
-           goTo={ref3}
-        />
-        <SideNav
-           side='left'
-           goTo={ref4}
-        />
-      </div>
-      <Projects pointer = {ref2} setModal={setModal}/>
-      <div ref={ref3} id="2" className='test' >3</div>
-      <div ref={ref4} id="3" className='test' >4</div>
-    </>
-  )
+	return (
+		<>
+			{modal && <Modal content={modal} closeModal={setModal} />}
+			<div
+				onClick={() => ref1.current.scrollIntoView({ behavior: 'smooth' })}
+				ref={ref1}
+			>
+				<Landing />
+			</div>
+			<div className="side-btns">
+				<SideNav side="right" goTo={ref2} />
+				<SideNav side="bottom" goTo={ref3} />
+				<SideNav side="left" goTo={ref4} />
+			</div>
+			<Projects pointer={ref2} setModal={setModal} />
+			<div ref={ref3} id="2" className="test">
+				3
+			</div>
+			<div ref={ref4} id="3" className="test">
+				4
+			</div>
+		</>
+	);
 }
 
-export default App
-
+export default App;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,48 +1,99 @@
-import { object } from "prop-types"
+import '../styles/projects.css';
+import Cube from './Cube';
+import { useState, useRef } from 'react';
+import { func, object } from 'prop-types';
 
-import '../styles/projects.css'
-import Cube from "./Cube"
-import { useState } from "react"
-import { func } from "prop-types"
+export default function Projects({ pointer, setModal }) {
+	const [showSide, setShowSide] = useState('cube show-bottom');
+	const [selectedSide, setSelectedSide] = useState(null);
 
-export default function Projects ({pointer, setModal}) {
-    const [showSide, setShowSide] = useState('cube show-bottom')
+	//changes sides from side menu
+	const handleChangeSide = (side, e) => {
+		const scene = document.getElementsByClassName('scene');
+		scene[0].className = 'scene scale';
+		setTimeout(function () {
+			scene[0].className = 'scene';
+		}, 1000);
+		// if(activeBtn) {
+		//     setActiveBtn(e.target.value)
+		// }
+		// console.log(e)
+		// e.target.className ='active'
+		// setActiveBtn(e.target)
+		setSelectedSide(side);
+		setShowSide('cube show-' + side);
+	};
 
-    //changes sides from side menu
-    const changeSide = (side) => {
-        const scene = document.getElementsByClassName('scene')
-        scene[0].className = 'scene scale'
-        setTimeout(function(){
-            scene[0].className = 'scene'
-        },1000)
-        setShowSide('cube show-' + side)
-    }
+	return (
+		<div className="projects-container" ref={pointer}>
+			<div className="cube-wrapper">
+				<Cube showSide={showSide} setModal={setModal} />
+			</div>
+			<div className="project-nav">
+				<div>
+					<label className="button-label">
+						<input
+							type="radio"
+							name="side"
+							value="left"
+							checked={selectedSide === 'left'}
+							onChange={(e) => handleChangeSide(e.target.value)}
+							className="visually-hidden"
+						/>
+						<span className="button-text">Bookshelf</span>
+					</label>
 
-
-    return (
-        <div 
-            className="projects-container"
-            ref={pointer}
-        >
-            <div className="cube-wrapper">
-                <Cube showSide={showSide} setModal={setModal}/>
-            </div>
-
-            <div className="project-nav">
-                <div>
-                    <button onClick={e=>changeSide(e.target.value)} value='left'>Bookshelf</button>
-                    <button onClick={e=>changeSide(e.target.value)} value='right'>CV maker</button>
-                    <button onClick={e=>changeSide(e.target.value)} value='front'>1 Coming soon...</button>
-                    <button onClick={e=>changeSide(e.target.value)} value='top'>2 coming soon...</button>
-                    {/* <button onClick={e=>changeSide(e.target.value)} value='bottom'>coming soon...</button> */}
-                    <button onClick={e=>changeSide(e.target.value)} value='back'>3 coming soon...</button>
-                </div>
-            </div>
-        </div>
-    )
+					<label className="button-label">
+						<input
+							type="radio"
+							name="side"
+							value="right"
+							checked={selectedSide === 'right'}
+							onChange={(e) => handleChangeSide(e.target.value)}
+							className="visually-hidden"
+						/>
+						<span className="button-text">CV maker</span>
+					</label>
+					<label className="button-label">
+						<input
+							type="radio"
+							name="side"
+							value="front"
+							checked={selectedSide === 'front'}
+							onChange={(e) => handleChangeSide(e.target.value)}
+							className="visually-hidden"
+						/>
+						<span className="button-text">1 Coming soon...</span>
+					</label>
+					<label className="button-label">
+						<input
+							type="radio"
+							name="side"
+							value="top"
+							checked={selectedSide === 'top'}
+							onChange={(e) => handleChangeSide(e.target.value)}
+							className="visually-hidden"
+						/>
+						<span className="button-text">2 coming soon...</span>
+					</label>
+					<label className="button-label">
+						<input
+							type="radio"
+							name="side"
+							value="back"
+							checked={showSide === 'back'}
+							onChange={(e) => handleChangeSide(e.target.value)}
+							className="visually-hidden"
+						/>
+						<span className="button-text">3 coming soon...</span>
+					</label>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 Projects.propTypes = {
-    pointer: object,
-    setModal:func
-}
+	pointer: object,
+	setModal: func,
+};
