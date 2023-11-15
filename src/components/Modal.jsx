d777f643ba34e1f46d343337177109535066409f
taskotale <110062804+taskotale@users.noreply.github.com>
2023-11-14 21:41:15 -0500

    made changes in directions

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -56,22 +56,29 @@ const parseContent = (contentObj) => {
 	}
 
 	return (
-		<div className="display-projects">
-			{contentObj.name && <h1>{contentObj.name}</h1>}
-			{contentObj.desc && (
-				<p className="project-description">{contentObj.desc}</p>
-			)}
-			{contentObj.tech && (
+		<div>
+			<div className="display-projects">
 				<div>
-					Tech Used:
-					<div className="tech-list">{techUsed}</div>
+					{contentObj.name && <h1>{contentObj.name}</h1>}
+					{contentObj.desc && (
+						<p className="project-description">{contentObj.desc}</p>
+					)}
+					{contentObj.tech && (
+						<div>
+							Tech Used:
+							<div className="tech-list">{techUsed}</div>
+						</div>
+					)}
 				</div>
-			)}
-			{contentObj.features && <ul className="project-features">Features: {features}</ul>}
-			{isValidHttpUrl(contentObj.link) && (
-				<a href={contentObj.link}>Check out the live version!</a>
-			)}
-			{!isValidHttpUrl(contentObj.link) && <a>Currently not deployed</a>}
+				<div>
+					{contentObj.features && <ul className="project-features">Features: {features}</ul>}</div>
+			</div>
+			<div>
+				{isValidHttpUrl(contentObj.link) && (
+					<a href={contentObj.link}>Check out the live version!</a>
+				)}
+				{!isValidHttpUrl(contentObj.link) && <a>Currently not deployed</a>}
+			</div>
 		</div>
 	);
 };
